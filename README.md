1e65cfdc0e0d19a73b182f2a0071872f920eea0f
nis-sin <nischalgurungji@gmail.com>
2024-09-12 18:06:56 +0100

    Feature and README add
    Added feature to parse field arguments separated by commas and check field argument validity.
    Added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a coding challenge from [Coding Challenges](https://codingchallenges.fyi/challenges/challenge-cut/) by John Crickett.
+
+---
+
+I am using the challenges to teach myself C because I am interested in low-level programming.
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <locale.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <ctype.h>
+#include <math.h>
 
 
 int main(int argc, char* argv[]) {
@@ -13,6 +15,7 @@ int main(int argc, char* argv[]) {
     char* fileName = argv[argc - 1];
     struct stat64 buf;
 
+    // Check if file exists
     if (stat64(fileName, &buf) == -1){
         printf("File does not exist\n");
         return 1;
@@ -21,17 +24,18 @@ int main(int argc, char* argv[]) {
     FILE* fp = fopen(fileName, "r");
 
     wint_t c;
-    char* fields;
+    char* fieldString;
     int arg;
     int foption = 0;
     unsigned int tabCount = 0;
     char t;
 
+    // Parse command line arguments
     while ((arg = getopt(argc, argv, "-f")) != -1){
         switch (arg){
             case 'f':
                 foption = 1;
-                fields = argv[optind];
+                fieldString = argv[optind];
                 break;
             case '?':
                 printf("error\n");
@@ -39,11 +43,44 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Parse fields string to obtain the fields number
 
+    unsigned int strptr = 0;
+    unsigned int maxLength = strlen(fieldString);
+    unsigned int field; 
+    unsigned int length; 
+    unsigned int fields[maxLength];
+    unsigned int index = 0;
+
+    memset(fields, 0, maxLength * sizeof(int));
+
+    while (strptr < maxLength){
+        field = strtol(fieldString, NULL, 10);
+
+        if (field == 0){
+            printf("Invalid field\n");
+            return 1;
+        }
+
+        length = floor(log10(field)) + 1;
+        fields[index] = field;
+
+        index++;
+        fieldString += length+1;
+        strptr += length+1;
+    }
+
+    for (int i = 0; i < maxLength; i++){
+        printf("%d\n", fields[i]);
+    }
+
+    return 0;
+
+    // Read file and print characters
     while ((c = fgetwc(fp)) != WEOF) {
 
         if (foption == 1){
-            if ((fields - 1) == tabCount){
+            if ((fieldString - 1) == tabCount){
                 printf("%lc", c);
             }
         }
